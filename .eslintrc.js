50ade1dead7b780236d440cbd736b6fc5c0af283
Markito97 <jetblade0@gmail.com>
2023-02-10 15:18:21 +0300

    обновил зависимости

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
 module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     es2021: true,
@@ -8,7 +10,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./client/tsconfig.json'],
+    project: './tsconfig.json',
   },
   plugins: ['react', 'html'],
   rules: {
