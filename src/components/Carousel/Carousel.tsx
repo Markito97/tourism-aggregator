6c41f471783cbd42a0e914faa5d942f50cd3bc88
Markito97 <jetblade0@gmail.com>
2023-02-10 09:56:20 +0300

    добавил альтернативный контент

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -7,57 +7,49 @@ import Lake3 from '../../assets/resource/Lake3.jpg'
 export const Carousel = (): JSX.Element => {
   return (
     <section className={styles.section}>
-      <div className={styles.contentItem}>
-        <div className={styles.imgWrapper}>
-          <img className={styles.contentImg} src={Lake1} alt="" />
+      <div className={styles.sectionUnit}>
+        <div className={styles.imageContainer}>
+          <img className={styles.unitImage} src={Lake1} alt="" />
         </div>
-        <div className={styles.itemText}>
-          <div className={styles.textContent}>
-            <div className={styles.textTitle}>Lake One</div>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-              ea asperiores quibusdam eveniet nesciunt illum fugiat, nulla illo
-              cumque praesentium voluptatem dolor facere expedita totam natus
-              accusantium officiis quas veniam?
-            </span>
+        <div className={styles.unitContent}>
+          <div className={styles.unitContentTitle}>Lake One</div>
+          <div className={styles.unitContentText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            assumenda officiis ratione ex, vero nostrum, aliquam repellat,
+            deleniti quas doloremque sit quasi culpa magnam exercitationem
+            voluptatem repellendus nihil consectetur perferendis.
           </div>
-          <button className={styles.itemBtn}>look</button>
+          <button className={styles.unitBtn}>Look</button>
         </div>
       </div>
-
-      <div className={styles.contentItemRevers}>
-        <div className={styles.itemText}>
-          <div>
-            <h1 className={styles.textTitleReverse}>Lake Two</h1>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-              ea asperiores quibusdam eveniet nesciunt illum fugiat, nulla illo
-              cumque praesentium voluptatem dolor facere expedita totam natus
-              accusantium officiis quas veniam?
-            </span>
-          </div>
-          <button className={styles.itemBtn}>look</button>
+      <div className={styles.sectionUnit}>
+        <div className={styles.imageContainer}>
+          <img className={styles.unitImage} src={Lake2} alt="" />
         </div>
-        <div className={styles.imgWrapper}>
-          <img className={styles.contentImg} src={Lake2} />
+        <div className={styles.unitContent}>
+          <div className={styles.unitContentTitle}>Lake One</div>
+          <div className={styles.unitContentText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            assumenda officiis ratione ex, vero nostrum, aliquam repellat,
+            deleniti quas doloremque sit quasi culpa magnam exercitationem
+            voluptatem repellendus nihil consectetur perferendis.
+          </div>
+          <button className={styles.unitBtn}>Look</button>
         </div>
       </div>
-
-      <div className={styles.contentItem}>
-        <div className={styles.imgWrapper}>
-          <img className={styles.contentImg} src={Lake3} alt="" />
+      <div className={styles.sectionUnit}>
+        <div className={styles.imageContainer}>
+          <img className={styles.unitImage} src={Lake3} alt="" />
         </div>
-        <div className={styles.itemText}>
-          <div className={styles.textContent}>
-            <h1 className={styles.textTitle}>Lake Three</h1>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
-              ea asperiores quibusdam eveniet nesciunt illum fugiat, nulla illo
-              cumque praesentium voluptatem dolor facere expedita totam natus
-              accusantium officiis quas veniam?
-            </span>
+        <div className={styles.unitContent}>
+          <div className={styles.unitContentTitle}>Lake One</div>
+          <div className={styles.unitContentText}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            assumenda officiis ratione ex, vero nostrum, aliquam repellat,
+            deleniti quas doloremque sit quasi culpa magnam exercitationem
+            voluptatem repellendus nihil consectetur perferendis.
           </div>
-          <button className={styles.itemBtn}>look</button>
+          <button className={styles.unitBtn}>Look</button>
         </div>
       </div>
     </section>
