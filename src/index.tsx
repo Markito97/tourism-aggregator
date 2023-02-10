2db4f24aac6f1a5be554a6ad50c2b7d9eff3ba6d
Markito97 <jetblade0@gmail.com>
2023-02-10 10:04:25 +0300

    переделал роутинг, чтобы использовать лоадеры из react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,5 @@
 import React from 'react'
 import App from './App'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+createRoot(document.getElementById('root')!).render(<App />)
--- src/routes/provider.tsx
+++ src/routes/provider.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { Layout } from '../components/Layout/Layout'
 import { Hotels } from '../pages/Hotels'
@@ -7,19 +12,19 @@ import { About } from '../pages/About'
 import { Contacts } from '../pages/Contacts'
 import { Activities } from '../pages/Activities'
 
-export const Provider = (): JSX.Element => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="hotels" element={<Hotels />} />
-          <Route path="hotels/:lake" element={<Hotels />} />
-          <Route path="aboutus" element={<About />} />
-          <Route path="activities" element={<Activities />} />
-          <Route path="contacts" element={<Contacts />} />
-        </Route>
-      </Routes>
-    </>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="hotels" element={<Hotels />} />
+      <Route path="hotels/:lake" element={<Hotels />} />
+      <Route path="aboutus" element={<About />} />
+      <Route path="activities" element={<Activities />} />
+      <Route path="contacts" element={<Contacts />} />
+    </Route>
   )
+)
+
+export const Provider = (): JSX.Element => {
+  return <RouterProvider router={router} />
 }
