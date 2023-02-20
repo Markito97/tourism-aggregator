5e4be19336a7c67f6ba4858c1ef7d402ec8a00ba
Markito97 <jetblade0@gmail.com>
2023-02-21 02:52:23 +0300

    изменил зависимости

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,17 +1,17 @@
-import * as path from 'path'
-import * as webpack from 'webpack'
-import 'webpack-dev-server'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
+import * as path from "path";
+import * as webpack from "webpack";
+import "webpack-dev-server";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const config: webpack.Configuration = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
   output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/dist/',
-    filename: 'bundle.js',
-    assetModuleFilename: 'images/[hash][ext][query]',
+    path: path.join(__dirname, "static"),
+    // publicPath: "/static/",
+    filename: "bundle.js",
+    assetModuleFilename: "images/[hash][ext][query]",
   },
   devServer: {
     client: {
@@ -25,21 +25,21 @@ const config: webpack.Configuration = {
         test: /\.css/i,
         use: [
           {
-            loader: 'style-loader',
+            loader: "style-loader",
           },
-          'css-loader',
+          "css-loader",
         ],
       },
       {
         test: /\.(png|jpg|gif)$/,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.tsx?$/,
-        loader: 'esbuild-loader',
+        loader: "esbuild-loader",
         options: {
-          loader: 'tsx',
-          target: 'esnext',
+          loader: "tsx",
+          target: "esnext",
         },
       },
     ],
@@ -50,22 +50,22 @@ const config: webpack.Configuration = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: "./public/index.html",
     }),
     new webpack.ProvidePlugin({
-      React: 'react',
-      ReactDOM: 'react-dom',
+      React: "react",
+      ReactDOM: "react-dom",
     }),
   ],
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, 'src/assets/resource/'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@components': path.resolve(__dirname, 'src/components/'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      "@assets": path.resolve(__dirname, "src/assets/resource/"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@components": path.resolve(__dirname, "src/components/"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
     },
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
-}
+};
 
-export default config
+export default config;
