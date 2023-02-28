48697df3e008aaf94cd3990aa1b8ff59de8b99e4
Alexander <alexanderlysenko99@mail.ru>
2023-03-01 00:29:43 +0300

    верстка формы, роутер не робит

--- frontend/admin/src/components/HouseForm/HouseForm.tsx
+++ frontend/admin/src/components/HouseForm/HouseForm.tsx
@@ -9,6 +9,9 @@ export interface IHouse {
   description: string;
   price: string;
   location: string;
+  testField: string;
+  checkin: string;
+  checkout: string;
 }
 
 export const HouseForm = observer(() => {
@@ -30,97 +33,154 @@ export const HouseForm = observer(() => {
   };
   return (
     <div className={styles.houseFormFields}>
-      <h1>Adding House</h1>
       <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <div className={styles.formItem}>
-          <label className={styles.textLable}>
-            Name
+        <div className={styles.leftFormPart}>
+          <div className={styles.formItem}>
+            <label className={styles.textLable}>NAME</label>
             <input
               className={styles.textField}
               placeholder="Name"
               {...register('name', {
                 required: 'This is required.',
-                minLength: {
-                  value: 5,
-                  message: 'Minimum 5 characters',
+                // minLength: {
+                // value: 5,
+                // message: 'Minimum 5 characters',
+                // },
+              })}
+            />
+
+            {errors?.name && (
+              <p className={styles.error}>{errors.name?.message}</p>
+            )}
+          </div>
+          <div className={styles.formItem}>
+            <label className={styles.textLable}>LOCATION</label>
+            <input
+              className={styles.textField}
+              placeholder="LOCATION"
+              {...register('location', {
+                required: 'This is required.',
+                maxLength: {
+                  value: 300,
+                  message: 'Maximum 300 characters',
                 },
               })}
             />
-          </label>
-
-          {errors?.name && (
-            <p className={styles.error}>{errors.name?.message}</p>
-          )}
-        </div>
-        <div className={styles.formItem}>
-          <label className={styles.textLable}>Description</label>
-          <textarea
-            className={styles.textarea}
-            placeholder="Description"
-            {...register('description', {
-              required: 'This is required.',
-              maxLength: {
-                value: 300,
-                message: 'Maximum 300 characters',
-              },
-            })}
-          />
-          {errors?.description && (
-            <p className={styles.error}>{errors.description?.message}</p>
-          )}
-        </div>
-
-        <div className={styles.formItem}>
-          <label className={styles.textLable}>Price</label>
-          <input
-            className={styles.textField}
-            placeholder="price"
-            {...register('price', {
-              required: 'This is required.',
-            })}
-          />
-          {errors?.price && (
-            <p className={styles.error}>{errors.price?.message}</p>
-          )}
-        </div>
-
-        <div className={styles.formItem}>
-          <label className={styles.textLable}>Location</label>
-          <input
-            className={styles.textField}
-            placeholder="Location"
-            {...register('location', {
-              required: 'This is required.',
-              maxLength: {
-                value: 300,
-                message: 'Maximum 300 characters',
-              },
-            })}
-          />
-          {errors?.location && (
-            <p className={styles.error}>{errors.location?.message}</p>
-          )}
-        </div>
-        <div className={styles.dragContainer}>
-          <div
-            className={styles.drag}
-            onDragStart={(e) => {
-              return dragStartHandler(e);
-            }}
-            onDragOver={(e) => {
-              return dragStartHandler(e);
-            }}
-            onDragLeave={(e) => {
-              return dragLeaveHandler(e);
-            }}
-            onDrop={(e) => {
-              return onDropHandler(e);
-            }}
-          >
-            Drop
+            {errors?.location && (
+              <p className={styles.error}>{errors.location?.message}</p>
+            )}
+          </div>
+          <div className={styles.formItem}>
+            <label className={styles.textLable}>SOME FIELD</label>
+            <input
+              className={styles.textField}
+              placeholder="SOME FIELD"
+              {...register('testField', {
+                required: 'This is required.',
+              })}
+            />
+            {errors?.price && (
+              <p className={styles.error}>{errors.price?.message}</p>
+            )}
+          </div>
+          <div className={styles.formItem}>
+            <label className={styles.textLable}>SOME FIELD</label>
+            <input
+              className={styles.textField}
+              placeholder="SOME FIELD"
+              {...register('price', {
+                required: 'This is required.',
+              })}
+            />
+            {errors?.price && (
+              <p className={styles.error}>{errors.price?.message}</p>
+            )}
+          </div>
+          <div className={styles.formItem}>
+            <label className={styles.textLable}>PRICE</label>
+            <input
+              className={styles.textField}
+              placeholder="PRICE"
+              {...register('price', {
+                required: 'This is required.',
+              })}
+            />
+            {errors?.price && (
+              <p className={styles.error}>{errors.price?.message}</p>
+            )}
           </div>
         </div>
-        <input type="submit" />
+        <div className={styles.rightFormPart}>
+          <div className={styles.formItem}>
+            <label className={styles.textLable}>FROM</label>
+            <input
+              className={styles.textField}
+              placeholder="CHECK-IN"
+              {...register('checkin', {
+                required: 'This is required.',
+              })}
+            />
+            {errors?.price && (
+              <p className={styles.error}>{errors.price?.message}</p>
+            )}
+          </div>
+          <div className={styles.formItem}>
+            <label className={styles.textLable}>TO</label>
+            <input
+              className={styles.textField}
+              placeholder="CHECK-OUT"
+              {...register('checkout', {
+                required: 'This is required.',
+              })}
+            />
+            {errors?.price && (
+              <p className={styles.error}>{errors.price?.message}</p>
+            )}
+          </div>
+
+          <div className={styles.formItem}>
+            <label className={styles.textLable}>Description</label>
+            <textarea
+              className={styles.textarea}
+              placeholder="Description"
+              {...register('description', {
+                required: 'This is required.',
+                maxLength: {
+                  value: 300,
+                  message: 'Maximum 300 characters',
+                },
+              })}
+            />
+            {errors?.description && (
+              <p className={styles.errorDescription}>
+                {errors.description?.message}
+              </p>
+            )}
+          </div>
+          <label className={styles.textLable}>IMAGES</label>
+          <div className={styles.dragContainer}>
+            <div
+              className={styles.drag}
+              onDragStart={(e) => {
+                return dragStartHandler(e);
+              }}
+              onDragOver={(e) => {
+                return dragStartHandler(e);
+              }}
+              onDragLeave={(e) => {
+                return dragLeaveHandler(e);
+              }}
+              onDrop={(e) => {
+                return onDropHandler(e);
+              }}
+            >
+              Drop
+            </div>
+          </div>
+          <button type="submit" className={styles.submitFormBtn}>
+            <span className={styles.btnText}>SUBMIT</span>
+          </button>
+        </div>
       </form>
     </div>
   );
--- frontend/admin/src/components/Sidebar/Sidebar.tsx
+++ frontend/admin/src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
 import styles from './Sidebar.module.css';
 
 export const Sidebar = (): JSX.Element => {
   return (
     <div className={styles.container}>
-      <div className={styles.option}>Houses</div>
-      <div className={styles.option}>Activities</div>
+      <div className={styles.option}> <Link to={'houseslist'}>Houses</Link></div>
+      <div className={styles.option}> <Link to={'activitieslist'}>Activities</Link></div>
     </div>
   );
 };
