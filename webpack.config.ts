7c522371e5545bda0aa162d05a63fed24f5a6816
Markito97 <jetblade0@gmail.com>
2023-03-19 12:01:45 +0300

    3

--- webpack.config.ts
+++ webpack.config.ts
@@ -24,7 +24,7 @@ module.exports = function (env: any): webpack.Configuration {
       historyApiFallback: true,
     },
     output: {
-      path: path.resolve(__dirname, 'static'),
+      path: path.resolve(__dirname, 'build'),
       filename: 'app.bundle.js',
     },
     resolve: {
