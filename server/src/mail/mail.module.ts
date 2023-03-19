33c0158f11ace1e07ef8c7b09b629acd2eae1af7
Markito97 <jetblade0@gmail.com>
2023-03-19 10:48:07 +0300

    fix package.json

--- server/src/mail/mail.module.ts
+++ server/src/mail/mail.module.ts
@@ -4,30 +4,6 @@ import { ConfigService } from "@nestjs/config";
 import { MailControler } from "./mail.controller";
 
 @Module({
-  imports: [
-    // MailerModule.forRoot({
-    //   transport: "smtps://user@domain.com:pass@smtp.domain.com",
-    //   template: {
-    //     dir: process.cwd() + "/templates/",
-    //     adapter: new HandlebarsAdapter(),
-    //     options: {
-    //       strict: true,
-    //     },
-    //   },
-    // }),
-    // MailerModule.forRootAsync({
-    //   useFactory: () => ({
-    //     transport: "smtps://user@domain.com:pass@smtp.domain.com",
-    //     template: {
-    //       dir: process.cwd() + "/templates/",
-    //       adapter: new HandlebarsAdapter(),
-    //       options: {
-    //         strict: true,
-    //       },
-    //     },
-    //   }),
-    // }),
-  ],
   providers: [MailService, ConfigService],
   controllers: [MailControler],
 })
